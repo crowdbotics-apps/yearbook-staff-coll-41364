7b5ed43958508c047990af652c66a749f6fe8235
Crowdbotics <team@crowdbotics.com>
2023-06-08 15:32:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yearbook-staff-coll-41364",
+  url: "https://yearbook-staff-coll-41364.botics.co",
+  api: "https://yearbook-staff-coll-41364.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
